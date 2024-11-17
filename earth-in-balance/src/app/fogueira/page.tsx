25e95c69ffcee0f9da41b0513bc0b02302de201c
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-11-17 20:29:11 -0300

    Fazendo o formulário de hábito

--- earth-in-balance/src/app/fogueira/page.tsx
+++ earth-in-balance/src/app/fogueira/page.tsx
@@ -1,7 +1,26 @@
 export default function fogueira(){
     return(
         <div>
-            <h2>Coloque aqui seus hábitos ruins que você quer colocar na fogueira</h2>
+            <h2>BEM VINDO A FOGUEIRA</h2>
+            <p>Aqui você pode nos falar um mau hábito que você quer deixar para trás e coloca-lo em nossa fogueira</p>
+            <form>
+                <h2>Coloque aqui seus hábitos ruins que você quer colocar na fogueira</h2>
+                <div>
+                    <label id="idDesc">Insira o seu mau hábito</label>
+                    <input type="text" placeholder="Mau hábito" id="idDesc" name="descricao" required/>
+                </div>
+                <div>
+                    <label id="idQtd">Insira a quantidade de dias na semana que você acha que isso ocorre</label>
+                    <input type="number" placeholder="Dias" id="idDesc" name="qtdDia" required/>
+                </div>
+                <div>
+                    <label id="idUser">Insira o identificador do seu cadastro</label>
+                    <input type="text" placeholder="Id do usuário" id="idUser" name="id_usuario" required/>
+                </div>
+                <div>
+                    <button type="submit">Colocar mau hábito na fogueira</button>
+                </div>
+            </form>
         </div>
     )
 }
