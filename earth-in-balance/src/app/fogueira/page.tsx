49542dd403b15cc669212edbeca78d79bc6cbb84
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-11-17 20:47:35 -0300

    usando handlechange e fazendo a validação de id_usuario

--- earth-in-balance/src/app/fogueira/page.tsx
+++ earth-in-balance/src/app/fogueira/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import { TipoHabito } from "@/types/types"
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function fogueira(){
 
@@ -11,7 +11,33 @@ export default function fogueira(){
         usuario:{
             id:0
         }
-    })
+    });
+
+    const handleChange = (evento:React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = evento.target;
+
+        setHabito((habito) =>{
+            if(name === "id_usuario"){
+                return{
+                    ...habito,
+                    usuario:{
+                        ...habito.usuario,
+                        id: Number(value),
+                    },
+                };
+            
+            }else{
+                return{
+                    ...habito,
+                    [name]:value,
+                };
+            }
+        });
+
+    }
+
+
+
     return(
         <div>
             <h2>BEM VINDO A FOGUEIRA</h2>
@@ -20,15 +46,15 @@ export default function fogueira(){
                 <h2>Coloque aqui seus hábitos ruins que você quer colocar na fogueira</h2>
                 <div>
                     <label id="idDesc">Insira o seu mau hábito</label>
-                    <input type="text" placeholder="Mau hábito" id="idDesc" name="descricao" value={habito.descricao} required/>
+                    <input type="text" placeholder="Mau hábito" id="idDesc" name="descricao" value={habito.descricao} onChange={(evento) => handleChange(evento)} required/>
                 </div>
                 <div>
                     <label id="idQtd">Insira a quantidade de dias na semana que você acha que isso ocorre</label>
-                    <input type="number" placeholder="Dias" id="idDesc" name="qtdDia" value={habito.qtdDia} required/>
+                    <input type="number" placeholder="Dias" id="idDesc" name="qtdDia" value={habito.qtdDia} onChange={(evento) => handleChange(evento)} required/>
                 </div>
                 <div>
                     <label id="idUser">Insira o identificador do seu cadastro</label>
-                    <input type="text" placeholder="Id do usuário" id="idUser" name="id_usuario" value={habito.usuario.id} required/>
+                    <input type="text" placeholder="Id do usuário" id="idUser" name="id_usuario" value={habito.usuario.id} onChange={(evento) => handleChange(evento)} required/>
                 </div>
                 <div>
                     <button type="submit">Colocar mau hábito na fogueira</button>
