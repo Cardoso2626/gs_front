8f3ad1a6472cb2fff0eaaf71eb96c49481bbf9b4
MarcosVini222 <lourencmarcos@gmail.com>
2024-11-17 16:14:28 -0300

    Arrumando o rodapé

--- earth-in-balance/src/components/Rodape/Rodape.tsx
+++ earth-in-balance/src/components/Rodape/Rodape.tsx
@@ -1,19 +1,20 @@
-export default function Rodape(){
-    return (
-        <footer className="bg-black text-white text-center py-5">
-            <div className="flex flex-wrap justify-around mx-auto p-5">
-                <div className="flex-1 min-w-[300px] m-2">
-                    <h3 className="mb-2">Contato</h3>
-                    <p>Email: heitor.ortega16@gmail.com</p>
-                    <p>Telefone: (11) 94743-1188</p>
-                </div>
-                <div className="flex-1 min-w-[300px] m-2">
-                    <h3 className="mb-2">Endereço</h3>
-                    <p>Av. Paulista, 1106</p>
-                    <p>Bela Vista, São Paulo</p>
-                    <p>CEP: 01311-000</p>
-                </div>
-            </div>
-        </footer>
-    );
-};
+import Logo from "@/img/logo.png";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Rodape() {
+  const currentYear = new Date().getFullYear(); 
+
+  return (
+    <footer className="bg-black text-white py-5">
+      <div className="flex flex-wrap justify-center mx-auto p-5">
+        <div className="flex items-center space-x-4">
+          <h3 className="mb-0">Earth in Balance © {currentYear}–{currentYear}</h3>
+          <Link href="/" className="flex items-center">
+            <Image src={Logo} alt="Earth in Balance Logo" width={60} height={60} />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
