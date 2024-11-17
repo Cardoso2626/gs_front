759d96b07af1d230c700eea395540932d85d07dc
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-11-17 15:09:33 -0300

    Fazendo o forms para o cadastro de usuário

--- earth-in-balance/src/app/cadastroUsuario/page.tsx
+++ earth-in-balance/src/app/cadastroUsuario/page.tsx
@@ -1,7 +1,29 @@
 export default function CadastroUsuario(){
     return(
         <div>
-            <h2>Cadastre-se aqui</h2>
+            <form>
+                <h2>Por um mundo mais sustentável</h2>
+                <h3>Faça seu cadastro</h3>
+                <div>
+                    <label id="idNome"></label>
+                    <input type="text" name="nome" id="idNome" placeholder="Nome do usuário" required />
+                </div>
+                <div>
+                    <label id="idIdade"></label>
+                    <input type="number" name="idade" id="idIdade" placeholder="idade" required min={0}/>
+                </div>
+                <div>
+                    <label id="idTelefone"></label>
+                    <input type="text" name="telefone" id="idTelefone" placeholder="Telefone" />
+                </div>
+                <div>
+                    <label id="idCpf" ></label>
+                    <input type="text" name="cpf" id="idCpf" placeholder="Cpf" required/>
+                </div>
+                <div>
+                    <button type="submit">Cadastre-se</button>
+                </div>
+            </form>
         </div>
     )
 }
