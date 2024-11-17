98ad5c550c060a483818983a44ed59b38b8b82bc
Cardoso2626 <sueleidesaraiva@hotmail.com>
2024-11-17 16:04:06 -0300

    Arrumando o menu junto com os links

--- earth-in-balance/src/components/Cabecalho/Cabecalho.tsx
+++ earth-in-balance/src/components/Cabecalho/Cabecalho.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/img/logo.png";
+import Menu from "@/components/Menu/Menu"
 
 export default function Cabecalho() {
   return (
@@ -9,37 +10,8 @@ export default function Cabecalho() {
         <Link href="/" className="flex items-center">
           <Image src={Logo} alt="Ícone da empresa" width={60} height={60} />
         </Link>
+        <Menu/>
         
-
-        <nav>
-          <ul className="flex space-x-6 text-sm font-medium text-gray-700">
-            <li>
-              <Link href="/" className="hover:text-teal-500 transition-colors">
-                Cadastro
-              </Link>
-            </li>
-            <li>
-              <Link href="/gastos" className="hover:text-teal-500 transition-colors">
-                Controle
-              </Link>
-            </li>
-            <li>
-              <Link href="/fogueira" className="hover:text-teal-500 transition-colors">
-                Fogueira
-              </Link>
-            </li>
-            <li>
-              <Link href="/planos" className="hover:text-teal-500 transition-colors">
-                Planos
-              </Link>
-            </li>
-            <li>
-              <Link href="/integrantes" className="hover:text-teal-500 transition-colors">
-                Integrantes
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
--- earth-in-balance/src/components/Menu/Menu.tsx
+++ earth-in-balance/src/components/Menu/Menu.tsx
@@ -3,13 +3,33 @@ import Link from "next/link";
 export default function Menu(){
     return(
         <nav>
-            <ul>
-                <li><Link href="/cadastroUsuario">Cadastre-se</Link></li>
-                <li><Link href="/integrantes">Integrantes</Link></li>
-                <li><Link href="/gastos">Gerenciamento de gastos</Link></li>
-                <li><Link href="/fogueira">Fogueira</Link></li>
-                <li><Link href="/planos">Planos</Link></li>
-            </ul>
+            <ul className="flex space-x-6 text-sm font-medium text-gray-700">
+            <li>
+              <Link href="/cadastroUsuario" className="hover:text-teal-500 transition-colors">
+                Cadastro
+              </Link>
+            </li>
+            <li>
+              <Link href="/gastos" className="hover:text-teal-500 transition-colors">
+                Controle
+              </Link>
+            </li>
+            <li>
+              <Link href="/fogueira" className="hover:text-teal-500 transition-colors">
+                Fogueira
+              </Link>
+            </li>
+            <li>
+              <Link href="/planos" className="hover:text-teal-500 transition-colors">
+                Planos
+              </Link>
+            </li>
+            <li>
+              <Link href="/integrantes" className="hover:text-teal-500 transition-colors">
+                Integrantes
+              </Link>
+            </li>
+          </ul>
         </nav>
     )
 }
