4c6414597f4770811e579938b368e973b307f5c3
HeitorOrtega <rm557825@fiap.com.br>
2024-11-21 22:31:43 -0300

    Arrumando responsividade do cabeçalho

--- earth-in-balance/src/components/Cabecalho/Cabecalho.tsx
+++ earth-in-balance/src/components/Cabecalho/Cabecalho.tsx
@@ -6,12 +6,23 @@ import Menu from "@/components/Menu/Menu";
 export default function Cabecalho() {
   return (
     <header className="bg-black border-b border-white">
-      <div className="container mx-auto flex justify-between items-center px-4 py-2">
+      <div className="container mx-auto flex flex-wrap items-center justify-between px-4 py-3">
         <Link href="/" className="flex items-center">
-          <Image src={Logo} alt="Ícone da empresa" width={60} height={60} />
-          <span className="ml-2 text-lg font-bold text-white  hover:text-green-300 transition-colors">Earth in Balance</span>
+          <Image
+            src={Logo}
+            alt="Ícone da empresa"
+            width={60}
+            height={60}
+            priority
+            className="h-auto w-auto"
+          />
+          <span className="ml-2 text-lg font-bold text-white hover:text-green-300 transition-colors">
+            Earth in Balance
+          </span>
         </Link>
-        <Menu />
+        <nav className="w-full md:w-auto mt-2 md:mt-0">
+          <Menu />
+        </nav>
       </div>
     </header>
   );
